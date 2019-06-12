e8ad162819d61c4408702bf345262e04e60322cd
1150625_LuisArteiro <1150625@isep.ipp.pt>
2019-06-12 23:30:00 +0100

    Socket.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
 import './css/imports.css'; // Import PostCSS files
 import configureStore from './store/configureStore';
 import Root from './components/Root';
 
 const devMode = process.env.NODE_ENV === 'development';
 const store = configureStore(devMode);
+
+let socket = io.connect('http://localhost:3000');
 
 ReactDOM.render(<Root store={store} />, document.getElementById('app'));
