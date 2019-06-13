d4d60ff1967c8190128813736d656c68bc9819b1
Fábio Lourenço <faboyds@gmail.com>
2019-06-13 20:53:46 +0100

    Trying to fix sockets server port AGAIN

--- src/index.jsx
+++ src/index.jsx
@@ -8,8 +8,7 @@ import Root from './components/Root';
 const devMode = process.env.NODE_ENV === 'development';
 const store = configureStore(devMode);
 
-
-const socket = io.connect('https://pixel-to-music.herokuapp.com:12554');
+const socket = io.connect(`https://pixel-to-music.herokuapp.com:${  process.env.PORT}`);
 
 /*
 send.addEventListener('click', () => {
